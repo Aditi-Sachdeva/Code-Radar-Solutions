8cf2a7006bbeacf64cef0f4554daa5d4f9cef13a
Aditi Sachdeva <aditisachdeva9159@gmail.com>
2025-03-30 15:59:14 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <limits.h> // For INT_MAX and INT_MIN
 
 void findMinDiffPair(int arr[], int n) {
     if (n < 2) {
         printf("-1");
         return;
     }
-
-    int minDiff = abs(arr[1] - arr[0]);
-    int pair1 = arr[0], pair2 = arr[1];
-
+    int min1 = INT_MAX, min2 = INT_MAX;
+    int max1 = INT_MIN, max2 = INT_MIN;
     for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            int diff = abs(arr[i] - arr[j]);
-            if (diff < minDiff) {
-                minDiff = diff;
-                pair1 = arr[i];
-                pair2 = arr[j];
-            }
+        if (arr[i] < min1) {
+            min2 = min1;
+            min1 = arr[i];
+        } else if (arr[i] < min2) {
+            min2 = arr[i];
+        }
+
+        if (arr[i] > max1) {
+            max2 = max1;
+            max1 = arr[i];
+        } else if (arr[i] > max2) {
+            max2 = arr[i];
         }
     }
 
-    printf("%d %d\n", pair1, pair2);
+    // Find the pair with the smallest difference
+    int diffMin = min2 - min1;
+    int diffMax = max1 - max2;
+
+    if (diffMin < diffMax)
+        printf("%d %d\n", min1, min2);
+    else
+        printf("%d %d\n", max1, max2);
 }
 
 int main() {
